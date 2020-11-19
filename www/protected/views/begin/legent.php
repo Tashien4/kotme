e2fab10528af720dc68c519c9aff28d55e5fcc6e
Tashien4 <50993301+Tashien4@users.noreply.github.com>
2020-11-20 01:10:54 +0500

    begin work for legent

--- www/protected/views/begin/legent.php
+++ www/protected/views/begin/legent.php
@@ -1,1 +1,51 @@
-еще не готово :(
+<?php $this->pageTitle=Yii::app()->name . ' - Login';?>
+<style>
+   .paper {
+        background: url("/kotme/www/images/for_game/paper.png") no-repeat;
+        background-size: contain;
+        background-repeat: no-repeat;
+        padding: 50px;
+
+        margin: 0;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
+    .text{ 
+        font-size: 20px;
+    padding: 40px;
+    font-weight: bold;
+    text-align: center;
+    margin: 20px;
+    line-height: 150%;  margin-top: -60px;
+       }
+    #btn {margin:10px;}
+    #btn a{
+    text-decoration: none;
+    cursor: pointer;
+    padding: 10px 20px;
+    font-weight: bold;
+    border-radius: 5px; 
+    background: #D59F13;
+    color: white;}
+</style>
+
+    <div class="paper">
+        
+        <div class="text">
+        Поиски древних сокровищ всегда интересовали
+         авантюристов всех мастей, с тех самых пор,
+         как эти сокровища укрывали - с глубокой древности.
+          Путешественник отправляется на остров для 
+          поиска древнего клада. Помоги путешественнику
+           расшифровать карту и найти сокровища. 
+           Двигайся по карте острова, решая задачи по Kotlin,
+           ищи подсказки и доберись до сокровищ.
+         </div>  
+	    <div id="btn"><a href="/kotme/www/index.php/begin/cart">Далее</a></div>
+			
+			
+        </div>	
+</div>
