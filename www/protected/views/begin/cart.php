6d6f1211defbafdf9c8ce6f8241bc3e0a8fee8ef
Tashien4 <50993301+Tashien4@users.noreply.github.com>
2020-12-07 12:41:03 +0500

    map

--- www/protected/views/begin/cart.php
+++ www/protected/views/begin/cart.php
@@ -1,13 +1,98 @@
 <style>
 #newfon{background-size: cover;    
   z-index: -2;}
-.char{font-size: 20px;
-    font-weight: bold;
-    text-align: center;
-    padding: 10px;
-    margin: 0px;
-    width: 100%;
-     }
+  .cart {
+        			background-image: url("/kotme/www/images/for_game/map/0.png");
+        			background-size: contain;
+        			background-repeat: no-repeat;
+  				width:675px;
+              			height:444px;
+              			position: absolute;
+        			left: 50%;
+        			top: 47%;
+    				text-align: center;
+       				transform: translate(-50%, -50%);	
+    			}
+          	
+          		.circle{
+          			width: 35px;
+              			height: 35px;
+              			border-radius: 50%;
+              			box-sizing: border-box;
+              			
+              			border-width:3px;
+              			border-style: solid;
+              			position: absolute;
+          		}
+          
+          		#lev1{
+            			display: block;
+            			width:36px;
+            			height: 36px;
+            			top: 64%;
+            			left: 30.5%;
+          		}
+          
+          		#lev2{
+            			display:block;
+            			top: 54.1%;
+            			left: 19%;
+          		}
+          
+          		#lev3{
+            			display:block;
+            		
+            			top: 39.4%;
+            			left:18.8%;
+          		}
+          
+          		#lev4{
+            			width:31px;
+            			height: 31px;
+          			top: 28%;
+            			left: 26%; 
+          		}
+          
+          		#lev5{
+            			width: 33px;
+            			height: 33px;
+          			top: 24.2%;
+          			left: 40.3%;
+          		}
+          
+          		#lev6{
+            			width: 31px;
+            			height: 31px;
+          			top: 25.4%;
+          			left: 51.8%;
+          		}
+          
+           		#lev7{
+            			width: 32px;
+            			height: 32px;
+          			top: 39.6%;
+          			left: 41.8%;
+          		}
+          
+          		#lev8{
+            			width: 34px;
+            			height: 34px;
+          			top: 53.7%;
+          			left: 49.6%;
+          		}
+          
+          		#lev9{
+          			top: 51%;
+          			left: 60.4%;
+         		}
+          
+          		#lev10{
+            			width:36px;
+            			height: 36px;
+          			top: 48.8%;
+            			left: 69.6%;
+            			border-width:4px;
+          		}
 footer{  display:flex;
   width: 100%;
     position: fixed; 
@@ -22,19 +107,29 @@ footer{  display:flex;
 
     div{text-align:center;}
     p{text-shadow: 2px 2px #a97311; color: #211603;    font-size: 30px;}
-
+.char{    font-size: 20px;
+    width: 80%;}
     </style>
-<?php $form=$this->beginWidget('CActiveForm');?>
-    <div>
-    <img src='/kotme/www/images/for_game/map/0.png' />
-    </div>
+<?php $form=$this->beginWidget('CActiveForm');
+$pr=Yii::app()->user->isProgress()?>
+<div class="cart">
+<?php
+  for($i=1;$i<11;$i++)
+    echo '<div id="lev'.$i.'" class="circle" style="'.
+          (($pr>=$i)?'background:rgba(95, 229, 11, 0.5);border-color:#5DB725;':
+          ((($pr+1)==$i)?'background:rgba(236, 240, 13, 0.5);
+          border-color:#ECF00D;':'display:none;')).'"
+          onclick="window.location.href=\'/kotme/www/index.php/begin/lessons?id='.$i.'\'"
+          ></div>';
+      ?>
+        </div>
 <footer> 
   <div class="char">
   <?php echo $model->getCharacterText(Yii::app()->user->isProgressChar())?><br><br>
   <?php echo  CHtml::submitButton('>>>',array('class'=>'btn','name'=>'next'))?>
     </div>
     <div class="icon">
-    <img id="character" src='/kotme/www/images/for_game/Character.png' width=75% />
+    <img id="character" src='/kotme/www/images/for_game/Character.png' width=75%/>
     </div>
 
 </footer>
