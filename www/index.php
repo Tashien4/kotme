98ab29193f71f3ae8d7efd4727a73648e567a52f
zeganstyl <zeganstyl@gmail.com>
2020-12-17 12:59:39 +0500

    composer

--- www/index.php
+++ www/index.php
@@ -1,8 +1,11 @@
 <?php
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../yii-current/framework/yii.php';
+$yii=dirname(__FILE__).'/../vendor/yiisoft/yii/framework/yii.php';
 
 $config=dirname(__FILE__).'/protected/config/main.php';
+
+// Add composer libs
+require __DIR__ . '/../vendor/autoload.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
